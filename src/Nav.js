d169a4bf40893bbbca38324aed3fa411ae3a5925
jf <jpf0628@gmail.com>
2022-06-30 13:42:26 -0700

    removed experience

--- src/Nav.js
+++ src/Nav.js
@@ -15,7 +15,7 @@ function Nav(){
        <HashLink className="Nav-font" to="#Bio-about-me">About</HashLink>
        <a className="Nav-font" href="https://github.com/JoHeyJo/myWebsiteReact/raw/main/resume.pdf"> Download Resume</a>
        <HashLink className="Nav-font" to="#footer-content">Contact</HashLink>
-       <HashLink className="Nav-font" to="#footer-content">Experience</HashLink>
+       {/* <HashLink className="Nav-font" to="#footer-content">Experience</HashLink> */}
       </BrowserRouter>
   </Navbar>
  ) 
