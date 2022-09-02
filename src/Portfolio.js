38f95ce5d9f4f51b966803804435f411aae93067
jf <jpf0628@gmail.com>
2022-09-02 12:35:01 -0700

    ADDED: warbler carousel item

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -5,6 +5,7 @@ import Carousel from 'react-bootstrap/Carousel';
 import memoryGameImg from "./img/memoryGameImg.png";
 import sharebbImg from "./img/sharebbImg.png";
 import joblyImg from "./img/joblyImg.png";
+import warblerImg from "./img/warblerImg.png";
 import "./Portfolio.css";
 import "./Header.css";
 
@@ -12,48 +13,61 @@ function Portfolio() {
   // const [activeSlide, setActiveSlide] = useState(0);
 
   return (
-      // <section id="portfolio-container">
-        <Carousel controls={false} className="imgCard" interval={null}>
-          <Carousel.Item>
-            <a href="https://memory-game-react-woad.vercel.app/">
-              <img
-                src={memoryGameImg}
-                alt="memory game slide"
-              />
-            </a>
-            <Carousel.Caption className="caption">
-              <h3>Memory Game</h3>
-              <p>Match randomly generated cards!</p>
-            </Carousel.Caption>
-          </Carousel.Item>
+    // <section id="portfolio-container">
+    <Carousel controls={false} className="imgCard" interval={null}>
+      <Carousel.Item>
+        <a href="https://memory-game-react-woad.vercel.app/">
+          <img
+            src={memoryGameImg}
+            alt="memory game slide"
+          />
+        </a>
+        <Carousel.Caption className="caption">
+          <h3>Memory Game</h3>
+          <p>Match randomly generated cards!</p>
+        </Carousel.Caption>
+      </Carousel.Item>
 
-          <Carousel.Item>
-            <a href="https://sharebb-fe.vercel.app/">
-              <img
-                src={sharebbImg}
-                alt="sharebb slide"
-              />
-            </a>
-            <Carousel.Caption className="caption">
-              <h3>AirBnB clone</h3>
-              <p>Create user, browse or add listings!</p>
-            </Carousel.Caption>
-          </Carousel.Item>
+      <Carousel.Item>
+        <a href="https://sharebb-fe.vercel.app/">
+          <img
+            src={sharebbImg}
+            alt="sharebb slide"
+          />
+        </a>
+        <Carousel.Caption className="caption">
+          <h3>AirBnB clone</h3>
+          <p>Create user, browse or add listings!</p>
+        </Carousel.Caption>
+      </Carousel.Item>
 
-          <Carousel.Item>
-            <a href="https://jobly-frontend.vercel.app/">
-              <img
-                src={joblyImg}
-                alt="jobly slide"
-              />
-            </a>
-            <Carousel.Caption className="caption">
-              <h3>Jobly: job listing board</h3>
-              <p>Create user and browse through job listings.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-      // </section>
+      <Carousel.Item>
+        <a href="https://jobly-frontend.vercel.app/">
+          <img
+            src={joblyImg}
+            alt="jobly slide"
+          />
+        </a>
+        <Carousel.Caption className="caption">
+          <h3>Jobly: job listing board</h3>
+          <p>Create user and browse through job listings.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <a href="https://warbler-warbler.herokuapp.com/signup">
+          <img
+            src={warblerImg}
+            alt="warbler slide"
+          />
+        </a>
+        <Carousel.Caption className="caption">
+          <h3>Warbler: Twitter clone </h3>
+          <p>Create user, follow user, add a message!</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    // </section>
   );
 }
 
