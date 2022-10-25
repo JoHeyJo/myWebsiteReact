e7de7fa01284859555add4875b64b8570d395122
jf <jpf0628@gmail.com>
2022-10-25 13:52:56 -0700

    styled Contact

--- src/Contact.js
+++ src/Contact.js
@@ -1,87 +1,93 @@
-// import React, { useState } from 'react';
-// import emailjs from '@emailjs/browser';
+import React, { useState } from 'react';
+import emailjs from '@emailjs/browser';
+import './Contact.css'
+import { Button } from 'react-bootstrap';
 
 // require('dotenv').config()
 
-// const SERVICE_ID = process.env.REACT_APP_SERVICE_KEY;
-// const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID;
-// const PUBLIC_KEY = process.env.REACT_APP_PUBLIC_KEY;
+const SERVICE_ID = process.env.REACT_APP_SERVICE_KEY;
+const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID;
+const PUBLIC_KEY = process.env.REACT_APP_PUBLIC_KEY;
 
-// const contactForm = {
-//   sender_name:'',
-//   sender_email:'',
-//   message:''
-// }
-
-// function Contact () {
-//   const [form, setForm] = useState(contactForm);
-
-//   function handleChange(e){
-//     e.preventDefault();
-
-//     const {name, value} = e.target
-//     setForm(formData => ({...formData, [name]: value}))
-//   }
-
-//   async function sendEmail(e) {
-//     e.preventDefault();
-//     try{
-//       const response = await emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY);
-//       console.log(response.text);
-//     } catch(error) {
-//       console.log(error.text);
-//     }
-//   };
-
-//   return (
-//     <form onSubmit={sendEmail}>
-//       <label htmlFor="sender-input">Name</label>
-//       <input onChange={handleChange} type="text" name="sender_name" />
-
-//       <label htmlFor='email=input'>Email</label>
-//       <input onChange={handleChange} type="email" name="sender_email" />
-
-//       <label htmlFor='message-input'>Message</label>
-//       <textarea onChange={handleChange} name="message" />
-//       <input type="submit" value="Send" />
-//     </form>
-//   );
-// };
-
-// export default Contact
-
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+const contactForm = {
+  from_name: '',
+  reply_to: '',
+  message: ''
+}
 
 function Contact() {
-  const form = useRef();
-  console.log('form', form)
-  console.log('form.current', form.current)
-  const sendEmail = (e) => {
+  const [form, setForm] = useState(contactForm);
+  const [alert, setAlert] = useState(null)
+
+  function handleChange(e) {
     e.preventDefault();
 
-    
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
-      .then((result) => {
-        console.log(result.text);
-      }, (error) => {
+    const { name, value } = e.target
+    setForm(formData => ({ ...formData, [name]: value }))
+  }
+
+  async function sendEmail(e) {
+    e.preventDefault();
+
+    if (!form.from_name || !form.reply_to || !form.message) {
+      setAlert('Please fill out all fields');
+    } else {
+
+      try {
+        const response = await emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, PUBLIC_KEY);
+        console.log(response.text);
+        setForm(contactForm);
+        setAlert('Thank you, your message has been sent!');
+      } catch (error) {
         console.log(error.text);
-      });
-    console.log('form', form)
-    console.log('form.current', form.current)
+        setForm(contactForm);
+        setAlert('Email was not sent please try, again.');
+      }
+    }
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <section id='contact-container'>
+
+      <form onSubmit={sendEmail}>
+
+        <div className='form-inputs'>
+          {/* <label className='sender-info' htmlFor="sender-name"></label> */}
+          <input
+            className='sender-info'
+            onChange={handleChange}
+            type="text"
+            name="from_name"
+            value={form.from_name}
+            placeholder='Name:' />
+        </div>
+
+        <div className='form-inputs'>
+          {/* <label className='sender-info' htmlFor='email-input'></label> */}
+          <input
+            className='sender-info'
+            onChange={handleChange}
+            type="email"
+            name="reply_to"
+            value={form.reply_to}
+            placeholder='Email:' />
+        </div>
+
+        <div className='form-inputs'>
+          <label htmlFor='message-input'></label>
+          <textarea
+            placeholder='Would love to hear your thoughts...'
+            rows={6}
+            onChange={handleChange}
+            name="message"
+            value={form.message} />
+          {/* <input id='submit-button' type="submit" value="Send" /> */}
+          <Button className='m-2' variant="outline-light">Send</Button>
+        </div>
+      </form>
+    </section>
   );
 };
 
-export default Contact;
+export default Contact
+
