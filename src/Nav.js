18efe86ece561a0fc2af5e533b9af38e756d5cc0
jf <jpf0628@gmail.com>
2023-01-28 10:29:48 -0800

    updated address link

--- src/Nav.js
+++ src/Nav.js
@@ -12,7 +12,7 @@ function Nav() {
       <HashLink className="Nav-font" to="#Bio-about-me">About</HashLink>
       <Link className="Nav-font" to="/portfolio">Portfolio</Link>
       <a className="Nav-font"
-        href="https://github.com/JoHeyJo/myWebsiteReact/raw/cf03aedf624be6c1071a5514abfee8f3d588314e/public/Joannes_Figueroa_Resume.pdf"
+        href="https://github.com/JoHeyJo/myWebsiteReact/raw/fixing_navbar/public/Joannes_Figueroa_Resume_FS.pdf"
       >Resume</a>
       <HashLink className="Nav-font" to="#footer-content">Contact</HashLink>
     </Navbar>
