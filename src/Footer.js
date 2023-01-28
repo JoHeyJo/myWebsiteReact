c8792a68affc69260a793a7425cf6e54142607aa
jf <jpf0628@gmail.com>
2023-01-28 10:18:30 -0800

    rearranged navbar

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
-import './Footer.css'
+import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Footer(){
   return (
@@ -7,6 +8,9 @@ function Footer(){
 
       <section id="footer-content">
         <p className="d-none d-lg-block">510-931-3588</p>
+        {/* <p><FontAwesomeIcon icon="fa-brands fa-instagram" /></p> */}
+        <FontAwesomeIcon icon="fa-brands fa-instagram" />
+        
         <p id="contact-text">contact</p>
         <p className="d-none d-lg-block">JPF0628@GMAIL</p>
       </section>
--- src/Nav.js
+++ src/Nav.js
@@ -10,11 +10,11 @@ function Nav() {
   return (
     <Navbar className="Nav-navbar d-none d-sm-flex fs-4">
       <HashLink className="Nav-font" to="#Bio-about-me">About</HashLink>
+      <Link className="Nav-font" to="/portfolio">Portfolio</Link>
       <a className="Nav-font"
         href="https://github.com/JoHeyJo/myWebsiteReact/raw/cf03aedf624be6c1071a5514abfee8f3d588314e/public/Joannes_Figueroa_Resume.pdf"
       >Resume</a>
       <HashLink className="Nav-font" to="#footer-content">Contact</HashLink>
-      <Link className="Nav-font" to="/portfolio">Portfolio</Link>
     </Navbar>
   )
 }
