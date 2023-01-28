212819c8cd243ba8ad0139dd641dfec4c785e779
jf <jpf0628@gmail.com>
2023-01-28 11:29:57 -0800

    new bio

--- src/Bio.js
+++ src/Bio.js
@@ -8,10 +8,9 @@ function Bio() {
       <ul className="fsm" id="about-me-links container">
         <li className="arrow">Hi! I'm Joannes.</li>
         <li>Welcome to my page!</li>
-        <li>11 year veteran of the bar industry</li>
-        <li>Pursuing my childhood dream to play with computers </li>
-        <li>Graduated from an immersive full stack web development program at Rithm School</li>
-        <li>I am a full stack developer and I want to build software that makes technology more accessible to everyone</li>
+        <li>I'm a Full Stack Engineer and I am motivated by the creative process that arises from solving problems. I have experience building user-friendly applications that abstract a robust architecture through an intuitive interface and building full stack applications from the ground up.</li>
+        <li>I want to write software and develop applications that have a user friendly mindset. My goal is to make tech more inclusive and accessible by contributing meaningful impact through thoughtful, creative programming . </li>
+        <li>In my previous life I was in the bar industry for 11 year and after attending a Full Stack Web Development program at Rithm School I was able to leverage my previous experience with the tools and resources gained at Rithm to be a successful software engineer.</li>
       </ul>
     </section>
   )
