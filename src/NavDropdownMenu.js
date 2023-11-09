c5fbf44d76ec97ff00960e93d6e3e406cfedf492
jf <jpf0628@gmail.com>
2023-11-08 21:20:59 -0800

    added links to drop down

--- src/NavDropdownMenu.js
+++ src/NavDropdownMenu.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
+import Nav from './Nav';
+import { HashLink } from "react-router-hash-link";
+import { Link } from "react-router-dom";
 
 function NavDropdownMenu() {
   return (
@@ -8,9 +11,12 @@ function NavDropdownMenu() {
       </Dropdown.Toggle>
 
       <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        <Dropdown.Item href="#/action-1"><HashLink className="Nav-font" to="#Bio-about-me">About</HashLink></Dropdown.Item>
+        <Dropdown.Item href="#/action-2"><Link className="Nav-font" to="/portfolio">Portfolio</Link></Dropdown.Item>
+        <Dropdown.Item href="#/action-3">      <a className="Nav-font"
+          href="https://github.com/JoHeyJo/myWebsiteReact/raw/main/public/Joannes_Figueroa_Resume.pdf"
+        >Resume</a></Dropdown.Item>
+        <Dropdown.Item href="#/action-3"><HashLink className="Nav-font" to="#footer-content">Contact</HashLink></Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   );
