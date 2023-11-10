ed1e5e58a67c38eb7960142d690cac168830f6eb
jf <jpf0628@gmail.com>
2023-11-09 21:46:51 -0800

    devicons are dynamically changes based on window size wip: icons are not properly placed in container. Icons create a new line when window gets too small

--- src/Banner.js
+++ src/Banner.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './Banner.css'
 import './fontSize.css'
 
 function renderFlask(windowWidth) {
   return windowWidth > 1595
     ? <span>Flask</span>
-    : <i class="devicon-flask-original colored"></i>
+    : <i class="devicon-flask-original"></i>
 }
 
 function renderPython(windowWidth) {
@@ -24,14 +24,21 @@ function renderTypeScript(windowWidth) {
   return windowWidth > 609
     ? <span>TypeScript<span className="pipes">|</span></span>
     : <span><i class="devicon-typescript-plain"></i><span className="pipes">|</span></span>
-
 }
 
 function Banner() {
+  const [width, setWidth] = useState(Infinity);
+  useEffect(()=>{
+    function handleResize(){
+      setWidth(window.innerWidth)
+    }
+    window.addEventListener("resize",handleResize)
+  })
+
   return (
     // <section className="Banner-container" id="Banner-text">
     <section className="Banner-container bannerFs">
-      fullstack <br />swe {renderTypeScript(600)}{renderReact(900)}{renderPython(1300)}{renderFlask(1500)}
+      fullstack <br />swe {renderTypeScript(width)}{renderReact(width)}{renderPython(width)}{renderFlask(width)}
     </section>
   )
 }
