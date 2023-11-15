50d32397fc1397d68809d431604628c554ee006e
jf <jpf0628@gmail.com>
2023-11-14 20:41:43 -0800

    additional responsive styles for smaller screens

---
+++ src/styles.js
@@ -0,0 +1,26 @@
+
+export const containerStyle = {
+  border: '1px solid #000',
+  padding: '10px',
+};
+
+export const rowStyle = {
+  border: '1px solid #000',
+  display: 'flex',
+};
+
+export const navColStyle = {
+  border: '1px solid #000',
+  padding: '10px',
+};
+
+export const mainColStyle = {
+  border: '1px solid #000',
+  padding: '10px',
+  // width: '100%',
+};
+
+export const sectionStyle = {
+  border: '1px solid #000',
+  padding: '10px',
+};
